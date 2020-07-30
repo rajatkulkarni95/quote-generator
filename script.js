dd2e34435932bff6cc580fb739891a4d73022395
Rajat Kulkarni <rajatkulkarni95@gmail.com>
2020-07-30 23:03:43 +0530

    Initial Push

---
+++ script.js
@@ -0,0 +1,19 @@
+const API_URL = "http://api.quotable.io/random";
+const quoteContent = document.getElementById("content");
+const quoteAuthor = document.getElementById("author");
+
+//Fetch Quotes from API
+function getRandomQuote() {
+  fetch(API_URL)
+    .then((response) => response.json())
+    .then(({ content, author }) => {
+      quoteContent.innerText = content;
+      quoteAuthor.innerText = `- ${author}`;
+    })
+    .catch((error) => console.log(error));
+}
+
+//On Window Load
+document.onload(getRandomQuote());
+
+//On Click of New Quote Button
